a458f55897a7faad769fdaa0abf2fdcb5ee852ef
Vuong Ngoc Thuan <vanthuan678310@gmail.com>
2025-09-24 15:47:43 +0700

    test storybook

---
+++ src/features/auth/components/login.stories.jsx
@@ -0,0 +1,13 @@
+import { Login } from './login';
+
+export default {
+  title: 'Features/Auth/Login',
+  component: Login,
+  parameters: {
+    layout: 'centered', 
+  },
+};
+
+export const Default = {
+  args: {},
+};
